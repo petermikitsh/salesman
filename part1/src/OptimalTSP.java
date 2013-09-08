8ab8f28cb64faad42d648b1bca638c006d94ab97
Peter Mikitsh <peter.mikitsh@gmail.com>
2013-09-08 17:38:09 -0400

    generate graph vertices and print coordinates

--- part1/src/OptimalTSP.java
+++ part1/src/OptimalTSP.java
@@ -1,14 +1,28 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
+import java.util.Set;
+
 class OptimalTSP {
 
+  private static LinkedHashMap<Integer,Integer> vertices;
+
 	public static void main(String[] args) {
+
     try {
       checkErrorConditions(args);
     } catch (IllegalArgumentException e) {
       System.out.println(e.getMessage());
     }
+
+    generateVertices(args);
+    printVertices();
+    
 	}
 
   private static void checkErrorConditions(String[] args) throws IllegalArgumentException {
+
     if (args.length != 2) {
       throw new IllegalArgumentException("Usage: java OptimalTSP n seed");
     }
@@ -25,5 +39,38 @@ class OptimalTSP {
     if (n < 1 || n > 13) {
       throw new IllegalArgumentException("Number of vertices must be between 1 and 13");
     }
+  }
+
+  private static void generateVertices(String[] args) {
+    
+    int n = Integer.parseInt(args[0]);
+    int seed = Integer.parseInt(args[1]);
+    Random randomX = new Random(seed);
+    Random randomY = new Random(seed*2);
+    vertices = new LinkedHashMap<Integer,Integer>(n);
+
+    while (vertices.size() < n) {
+      int x = randomX.nextInt(n);
+      int y = randomY.nextInt(n);
+
+      if (vertices.containsKey(x)) {
+        continue;
+      } else {
+        vertices.put(x, y);
+      }
+    }
+  }
+
+  private static void printVertices() {
+
+    System.out.println("X-Y Coordinates:");
+
+    int index = 0;
+    for (Map.Entry<Integer, Integer> entry : vertices.entrySet()) {
+      System.out.format("v%d: (%d,%d) ", index, entry.getKey(), entry.getValue());
+      index++;
+    }
+
+    System.out.println();
   }
 }
