a5e962df7f5cc74fe25cab3f882fabf9618437d7
petermikitsh <peter.mikitsh@gmail.com>
2013-11-02 19:40:05 -0400

    project 3 base

---
+++ part3/src/Logger.java
@@ -0,0 +1,116 @@
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/* Logger
+
+   author: Peter Mikitsh pam3961
+*/
+class Logger {
+
+	private int n;
+  private static DecimalFormat df = new DecimalFormat("0.00");
+
+  /* Constructor. */
+	public Logger(String[] args) {
+		checkErrorConditions(args);
+	}
+
+  /* Fail early by checking error conditions from Project specifications. */
+  private void checkErrorConditions(String[] args) {
+
+    try {
+
+      if (args.length != 2) {
+        throw new IllegalArgumentException("Usage: java GreedyTSP n seed");
+      }
+
+      for (int i = 0; i < 2; i++) {
+        try {
+          Integer.parseInt(args[i]);
+        } catch (NumberFormatException e) {
+          throw new IllegalArgumentException("Command line args must be integers");
+        }
+      }
+
+      n = (int) Integer.parseInt(args[0]);
+      if (n < 1) {
+        throw new IllegalArgumentException("Number of vertices must be greater than 0");
+      }
+
+    } catch (IllegalArgumentException e) {
+
+      System.out.println(e.getMessage());
+      System.exit(0);
+
+    }
+  }
+
+  /* Prints graph vertex names with associated cartesian points. */
+	public void logCoordinates(LinkedHashMap<Integer,Integer> vertices) {
+		if (n <= 10) {
+
+  		System.out.println("X-Y Coordinates:");
+
+      int index = 0;
+      for (Map.Entry<Integer, Integer> entry : vertices.entrySet()) {
+        System.out.format("v%d: (%d,%d) ", index, entry.getKey(), entry.getValue());
+        index++;
+      }
+
+      System.out.println("\n");
+
+		}
+	}
+
+  /* Prints an adjacency matrix, with row and column labels. */
+	public void logAdjacencyMatrix(double[][] adjMtrx) {
+    if (n <= 10) {
+      System.out.printf("Adjacency matrix of graph weights:\n\n\t");
+
+      int i;
+      for (i = 0; i < adjMtrx.length; i++) {
+        System.out.printf("  %d\t", i);
+      }
+      System.out.println("\n");
+
+      for (i = 0; i < adjMtrx.length; i++) {
+        System.out.printf("%d\t", i);
+        for (double distance : adjMtrx[i]) {
+          System.out.printf("%s\t", df.format(distance));
+        }
+        System.out.println("\n");
+      }
+    }
+  }
+
+  public void logAdjacencyMatrixMST(double[][] adjMtrx) {
+    System.out.println("Minimum Spanning Tree:");
+    logAdjacencyMatrix(adjMtrx);
+  }
+
+  /* Prints the optimal solution distance and path traversal. */
+	public void logOptimalPath(double distance, List<Integer> path) {
+    System.out.printf("\nDistance using mst: %s for path ", df.format(distance));
+    for (Integer i : path)
+      System.out.printf("%d ", i);
+    System.out.println("");
+  }
+
+  /* Prints the runtime. */
+	public void logRuntime(long time) {
+    System.out.printf("Runtime for Mst TSP   : %s milliseconds\n", time);
+  }
+
+  /* Prints ordered list of edges from greedy tour. */
+  public void logEdgeTour(List<Edge> tour) {
+    if (n <= 10) {
+      System.out.println("Pre-order traversal: ");
+      for (Edge e : tour)
+        System.out.printf("%d %d weight = %s\n", e.column, e.row, df.format(e.getWeight()));
+      }
+  }
+
+}
---
+++ part3/src/MstTSP.java
@@ -0,0 +1,108 @@
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+/* GreedyTSP.java
+
+   author: Peter Mikitsh pam3961
+*/
+class MstTSP {
+
+  private static LinkedHashMap<Integer,Integer> vertices;
+  private static Graph gInitial;
+  private static Graph gGreedy;
+  private static Logger logger;
+  private static int n;
+  private static int seed;
+
+  /* - Initialize data structures for use.
+     - Times the execution of the greedy algorithm.
+     - Passes the results to the logger.
+  */
+	public static void main(String[] args) {
+
+    logger = new Logger(args);
+    n = (int) Integer.parseInt(args[0]);
+    seed = (int) Integer.parseInt(args[1]);
+    vertices = new LinkedHashMap<Integer,Integer>(n);
+    gInitial = new Graph(n);
+    gGreedy = new Graph(n);
+    gGreedy.initEmptyWeights();
+    
+    long startTime = System.currentTimeMillis();
+    generateVertices();
+    generateAdjacencyMatrix();
+    generateEdges();
+    greedyAlgorithm();
+    long endTime = System.currentTimeMillis();
+
+    logger.logCoordinates(vertices);
+    logger.logAdjacencyMatrix(gInitial.getMatrix());
+    logger.logAdjacencyMatrixMST(gGreedy.getMatrix());
+    logger.logEdgeTour(gGreedy.getEdges());
+    logger.logOptimalPath(gGreedy.calculateDistance(), gGreedy.dfsTraversal());
+    logger.logRuntime(endTime - startTime);
+    
+	}
+
+  /* Generate Cartesian points for vertices, per Part 1 specifications. */
+  private static void generateVertices() {
+    
+    Random randomX = new Random(seed);
+    Random randomY = new Random(seed*2);
+
+    while (vertices.size() < n) {
+
+      int x = randomX.nextInt(n);
+      int y = randomY.nextInt(n);
+
+      if (vertices.containsKey(x))
+        continue;
+      else
+        vertices.put(x, y);
+      
+    }
+  }
+
+  /* Fills in the Graph's adjacency matrix with appropriate weights. */
+  private static void generateAdjacencyMatrix() {
+
+    List<Map.Entry<Integer, Integer>> entries =
+      new ArrayList<Map.Entry<Integer, Integer>>(vertices.entrySet());
+
+    for (int x = 0; x < entries.size(); x++) {
+      for (int y = 0; y < entries.size(); y++) {
+        double weight = calculateVertexDistance(entries.get(x), entries.get(y));
+        gInitial.addEdgeWeight(x, y, weight);
+      }
+    }
+  }
+
+  /* Store the edges in an array, to make my life simpler. */
+  private static void generateEdges() {
+    for (int row = 0; row <= n-1; row++) {
+      for (int column = 0; column <= row-1; column++) {
+        gInitial.addEdge(row, column);
+      }
+    }
+  }
+
+  /* Performs a greedy, approximately optimal tour. */
+  public static void greedyAlgorithm() {
+
+  }
+
+  /* Use the distance formula to find the distance between to Cartesian points (vertices) */
+  private static Double calculateVertexDistance(Map.Entry<Integer,Integer> v1, Map.Entry<Integer,Integer> v2) {
+    
+    int x1 = v1.getKey();
+    int x2 = v2.getKey();
+    int y1 = v1.getValue();
+    int y2 = v2.getValue();
+
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)) ; 
+  }
+
+}
